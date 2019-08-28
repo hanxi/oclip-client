f55e0fd10a6630ba4b4351bbbc499bb2636cbfd8
涵曦 <hanxi.info@gmail.com>
2019-08-28 23:28:52 +0800

    clean files

--- platform/win/tmp/oclip.lua
+++
@@ -1,7 +0,0 @@
--- Version of bin/oclip.lua for use in oclip binaries.
-
--- Do not load modules from filesystem in case a bundled module is broken.
-package.path = ""
-package.cpath = ""
-
-require "oclip.main"
